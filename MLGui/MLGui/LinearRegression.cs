41b965e1dbc7521b41ef5514f84d7bbea16214dc
Isaac <lee3650@purdue.edu>
2020-10-17 13:06:07 -0400

    Added two models and UI for the regression page

---
+++ MLGui/MLGui/LinearRegression.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLGui
+{
+    class LinearRegression : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Directory
+        {
+            get;
+            set;
+        }
+
+        public string Data
+        {
+            get;
+            set;
+        }
+
+        public bool SelectModel
+        {
+            get;
+            set;
+        }
+
+        //The downside of this is if it's a path it's not going to work
+        //So, this is an optional string for the model if select model is checked. 
+        public string ModelPath
+        {
+            get;
+            set;
+        }
+
+        public RegressionModel Model
+        {
+            get;
+            set;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ MLGui/MLGui/RegressionModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLGui
+{
+    class RegressionModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string dependentColumn;
+        string independentColumn;
+        string dependentRow;
+        string independentRow;
+
+        public string DependentColumn
+        {
+            get
+            {
+                return dependentColumn;
+            }
+            set
+            {
+                dependentColumn = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
